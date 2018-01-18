10b3539de166ca51cef9569c09ad4f5c8b3eabef
ali.abbas <ali.abbas@tenpearls.com>
2018-01-18 15:59:16 +0500

    console log added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let http = require('http');
 
-
+console.log("starting server");
 http.createServer((request,response) => {
+    console.log("Request received");
     response.writeHead(200,{"Content-Type": "text/plain"});
     response.write("test response");
     response.end();
 }).listen(8080);
+
+console.log("Server started");
